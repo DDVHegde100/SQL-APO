9531f983237ebb3d028180c0fb24d42b046f16d0
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-07-30 10:45:42 -0400

    Add files via upload

---
+++ main (3).sql
@@ -0,0 +1,10 @@
+create table project_details (name varchar2(100), 
+description varchar2(255), 
+availability char, 
+price float;
+
+insert into project_details values ('RFID Door Unlock', 'RFID Card', 'y', 70)
+insert into project_details values ('Hands-Free Servo', 'Motion Unlock', 'y', 30)
+insert into project_details values ('Book Organizer', 'Alphabetical Organizer of Books, with Mehcanical Arm', 'y', 170)
+insert into project_details values ('Window-Roller Blinds', 'Through motion or signal, blinds can be moved.', 'y', 15)
+insert into project_details values ('Automatic Plant Watering', 'Servo turns water resovoir to pour water at specific intervals.', 'y', 25)
